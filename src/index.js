5e3f8c5c8454bb7672cb63213c6b6e7536fdd2b2
Aishwaryagunjal6 <gunjalaishwarya625@gmail.com>
2025-01-01 22:53:09 +0530

    Connect to MongoDB Atlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require(dotenv).config({path:'./env'}   this is not a good practice, hence the other approach is used
+
+import dontenv from 'dotenv'
+// import mongoose from 'mongoose'
+// import { DB_NAME } from './constants'
+import connectDB from './db/db.js'
+
+dontenv.config({ path: './env' })
+
+//first approach to connect database to the project
+/*
+import express from 'express'
+const app = express()
+
+(async()=>{
+  try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error)=>{
+      console.log("ERROR: ", error)
+      throw error
+    })
+
+    app.listen(process.env.PORT, ()=>{
+      console.log(`App is running on port: ${process.env.PORT}`)
+    })
+  }
+  catch(error){
+    console.error("ERROR! Failed to fetch data", error)
+  }
+})()  //database jab be connect karo hamesha async awit aur try catch ka use kro because data fetching requires time hence the code execution should wait until all the data is fetched
+  */
+
+
+//second approach is to import the connectDB function from db.js
+
+connectDB()
